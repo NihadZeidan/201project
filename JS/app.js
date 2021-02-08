f98ef8ef7054dac6266beb0993f8c04ca9c2b1b1
NihadZeidan <nihadzeidan@gmail.com>
2021-02-08 16:37:07 +0200

    Adding CSS and JS to my html

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,129 @@
 'use strict';
 
+let userName = prompt('Please enter your name:');
+alert('Nice to see you here ' + userName + ', Welcome!');
+alert('We will play a gussing yes/no game, before you enter my page. I hope you\'re ready and excited!');
+
+
+let education = prompt('Am I studied Business in my bechlor degree?');
+// console.log(education);
+
+while (education !== 'yes' && education !== 'y' && education !== 'no' && education !== 'n') {
+    education = prompt('please enter only yes/no or y/n');
+}
+
+switch (education.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('That\'s correct');
+        break;
+
+    case 'no':
+    case 'n':
+        alert('Sorry, I guess we need to know each other more');
+        break;
+
+    default:
+        alert('please enter only yes/no or y/n');
+        break;
+}
+
+
+
+let color = prompt('Is black my favourite color?');
+// console.log(color);
+
+while (color !== 'yes' && color !== 'y' && color !== 'no' && color !== 'n') {
+    color = prompt('please enter only yes/no or y/n');
+}
+
+switch (color.toLocaleLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('That\'s correct');
+        break;
+
+    case 'no':
+    case 'n':
+        alert('Sorry, I guess we need to know each other more');
+        break;
+
+    default:
+        alert('please enter only yes/no or y/n');
+        break;
+}
+
+let hobby = prompt('Is writing my favourite hobby?');
+// console.log(hobby); 
+
+while (hobby !== 'yes' && hobby !== 'y' && hobby !== 'no' && hobby !== 'n') {
+    hobby = prompt('please enter only yes/no or y/n');
+}
+
+switch (hobby.toLocaleLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('That\'s correct');
+        break;
+
+    case 'no':
+    case 'n':
+        alert('Sorry, I guess we need to know each other more');
+        break;
+
+    default:
+        alert('please enter only yes/no or y/n');
+        break;
+}
+
+
+let sport = prompt('Is football my favourite sport?');
+// console.log(sport);
+
+while (sport !== 'yes' && sport !== 'y' && sport !== 'no' && sport !== 'n') {
+    sport = prompt('please enter only yes/no or y/n');
+}
+
+switch (sport.toLocaleLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('Sorry, I guess we need to know each other more');
+        break;
+
+    case 'no':
+    case 'n':
+        alert('you\'re correct, my favourite sport is MMA and Boxing');
+        break;
+
+    default:
+        alert('please enter only yes/no or y/n');
+        break;
+}
+
+
+let hight = prompt('Is my hight more than 180cm? ');
+// console.log(hight);
+
+
+while (hight !== 'yes' && hight !== 'y' && hight !== 'no' && hight !== 'n') {
+    hight = prompt('please enter only yes/no or y/n');
+}
+
+switch (hight.toLocaleLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('you\'re correct, my hight is 190cm :D ');
+        break;
+
+    case 'no':
+    case 'n':
+        alert('Sorry, I guess we need to know each other more');
+        break;
+
+    default:
+        alert('please enter only yes/no or y/n');
+        break;
+}
+
+
+alert('Thank you ' + userName + ', you can take a look to my page now.');
