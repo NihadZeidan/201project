0fb5c9a1fe0c0f0716160c08aa5435e95211ca65
NihadZeidan <nihadzeidan@gmail.com>
2021-02-12 11:40:08 +0200

    uncomment Js

--- JS/app.js
+++ JS/app.js
@@ -1,152 +1,153 @@
-// 'use strict';
-// let score = 0
-// let userName = prompt('Please enter your name:');
-// alert('Nice to see you here ' + userName + ', Welcome!');
-// alert('We will play a gussing yes/no game, before you enter my page. I hope you\'re ready and excited!');
+'use strict';
+let score = 0
+let userName = prompt('Please enter your name:');
+alert('Nice to see you here ' + userName + ', Welcome!');
+alert('We will play a gussing yes/no game, before you enter my page. I hope you\'re ready and excited!');
 
 
-// function education() {
-//     let education = prompt('Am I studied Business in my bechlor degree?');
+function education() {
+    let education = prompt('Am I studied Business in my bechlor degree?');
 
-//     while (education !== 'yes' && education !== 'y' && education !== 'no' && education !== 'n') {
-//         education = prompt('please enter only yes/no or y/n');
-//     }
+    while (education !== 'yes' && education !== 'y' && education !== 'no' && education !== 'n') {
+        education = prompt('please enter only yes/no or y/n');
+    }
 
-//     switch (education.toLowerCase()) {
-//         case 'yes':
-//         case 'y':
-//             alert('That\'s correct');
-//             score++
-//             break;
+    switch (education.toLowerCase()) {
+        case 'yes':
+        case 'y':
+            alert('That\'s correct');
+            score++
+            break;
 
-//         case 'no':
-//         case 'n':
-//             alert('Sorry, I guess we need to know each other more');
-//             break;
+        case 'no':
+        case 'n':
+            alert('Sorry, I guess we need to know each other more');
+            break;
 
-//         default:
-//             alert('please enter only yes/no or y/n');
-//             break;
-//     }
-// }
-// education();
-
-
+        default:
+            alert('please enter only yes/no or y/n');
+            break;
+    }
+}
+education();
 
 
-// function color() {
-//     let color = prompt('Is black my favourite color?');
-
-//     while (color !== 'yes' && color !== 'y' && color !== 'no' && color !== 'n') {
-//         color = prompt('please enter only yes/no or y/n');
-//     }
-
-//     switch (color.toLocaleLowerCase()) {
-//         case 'yes':
-//         case 'y':
-//             alert('That\'s correct');
-//             score++
-//             break;
-
-//         case 'no':
-//         case 'n':
-//             alert('Sorry, I guess we need to know each other more');
-//             break;
-
-//         default:
-//             alert('please enter only yes/no or y/n');
-//             break;
-//     }
-// }
-// color();
-
-// function favHobby() {
-//     let hobby = prompt('Is writing my favourite hobby?');
-
-//     while (hobby !== 'yes' && hobby !== 'y' && hobby !== 'no' && hobby !== 'n') {
-//         hobby = prompt('please enter only yes/no or y/n');
-//     }
-
-//     switch (hobby.toLocaleLowerCase()) {
-//         case 'yes':
-//         case 'y':
-//             alert('That\'s correct');
-//             score++
-//             break;
-
-//         case 'no':
-//         case 'n':
-//             alert('Sorry, I guess we need to know each other more');
-//             break;
-
-//         default:
-//             alert('please enter only yes/no or y/n');
-//             break;
-//     }
-// }
-// favHobby();
-
-// function favSport() {
-//     let sport = prompt('Is football my favourite sport?');
-
-//     while (sport !== 'yes' && sport !== 'y' && sport !== 'no' && sport !== 'n') {
-//         sport = prompt('please enter only yes/no or y/n');
-//     }
-
-//     switch (sport.toLocaleLowerCase()) {
-//         case 'yes':
-//         case 'y':
-//             alert('Sorry, I guess we need to know each other more');
-//             break;
-
-//         case 'no':
-//         case 'n':
-//             alert('you\'re correct, my favourite sport is MMA and Boxing');
-//             score++
-//             break;
-
-//         default:
-//             alert('please enter only yes/no or y/n');
-//             break;
-//     }
-// }
-// favSport();
 
 
-// function myHeight() {
-//     let height = prompt('Is my height more than 180cm? ');
+function color() {
+    let color = prompt('Is black my favourite color?');
+
+    while (color !== 'yes' && color !== 'y' && color !== 'no' && color !== 'n') {
+        color = prompt('please enter only yes/no or y/n');
+    }
+
+    switch (color.toLocaleLowerCase()) {
+        case 'yes':
+        case 'y':
+            alert('That\'s correct');
+            score++
+            break;
+
+        case 'no':
+        case 'n':
+            alert('Sorry, I guess we need to know each other more');
+            break;
+
+        default:
+            alert('please enter only yes/no or y/n');
+            break;
+    }
+}
+color();
+
+function favHobby() {
+    let hobby = prompt('Is writing my favourite hobby?');
+
+    while (hobby !== 'yes' && hobby !== 'y' && hobby !== 'no' && hobby !== 'n') {
+        hobby = prompt('please enter only yes/no or y/n');
+    }
+
+    switch (hobby.toLocaleLowerCase()) {
+        case 'yes':
+        case 'y':
+            alert('That\'s correct');
+            score++
+            break;
+
+        case 'no':
+        case 'n':
+            alert('Sorry, I guess we need to know each other more');
+            break;
+
+        default:
+            alert('please enter only yes/no or y/n');
+            break;
+    }
+}
+favHobby();
+
+function favSport() {
+    let sport = prompt('Is football my favourite sport?');
+
+    while (sport !== 'yes' && sport !== 'y' && sport !== 'no' && sport !== 'n') {
+        sport = prompt('please enter only yes/no or y/n');
+    }
+
+    switch (sport.toLocaleLowerCase()) {
+        case 'yes':
+        case 'y':
+            alert('Sorry, I guess we need to know each other more');
+            break;
+
+        case 'no':
+        case 'n':
+            alert('you\'re correct, my favourite sport is MMA and Boxing');
+            score++
+            break;
+
+        default:
+            alert('please enter only yes/no or y/n');
+            break;
+    }
+}
+favSport();
 
 
-//     while (height !== 'yes' && height !== 'y' && height !== 'no' && height !== 'n') {
-//         height = prompt('please enter only yes/no or y/n');
-//     }
+function myHeight() {
+    let height = prompt('Is my height more than 180cm? ');
 
-//     switch (height.toLocaleLowerCase()) {
-//         case 'yes':
-//         case 'y':
-//             alert('you\'re correct :D ');
-//             score++
-//             break;
 
-//         case 'no':
-//         case 'n':
-//             alert('Sorry, I guess we need to know each other more');
-//             break;
+    while (height !== 'yes' && height !== 'y' && height !== 'no' && height !== 'n') {
+        height = prompt('please enter only yes/no or y/n');
+    }
 
-//         default:
-//             alert('please enter only yes/no or y/n');
-//             break;
-//     }
-// }
-// myHeight();
+    switch (height.toLocaleLowerCase()) {
+        case 'yes':
+        case 'y':
+            alert('you\'re correct :D ');
+            score++
+            break;
 
-// alert('Welcome to my page ' + userName);
+        case 'no':
+        case 'n':
+            alert('Sorry, I guess we need to know each other more');
+            break;
+
+        default:
+            alert('please enter only yes/no or y/n');
+            break;
+    }
+}
+myHeight();
+
+alert('Welcome to my page ' + userName);
 
 
 function queHeight() {
     let exactHeight = prompt('What do you think my exact height is? (insert a number)');
     let check = true;
     let attemptsAvailable = 3
+
 
     for (let i = 0; i <= 2; i++) {
         if (exactHeight > 190) {
@@ -167,6 +168,8 @@ function queHeight() {
         alert('In case you did not get it, the correct answer is 190 cm :D ');
     }
 }
+
+
 queHeight();
 
 
